0cabca44c697dfbc3a8e43d274e658743d6cf5a6
Ekhynox <60545676+Ekhynox@users.noreply.github.com>
2021-02-17 16:21:07 +0100

    Pictionary

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import './App.css';
+import React, {useState} from 'react';
+import reactCSS from 'reactcss';
+import {CirclePicker} from 'react-color';
+import {CompactPicker} from 'react-color';
+import {SwatchesPicker} from 'react-color';
+import {GithubPicker} from 'react-color';
+import {ColorChange} from './index';
+
+
+function App() {
+  //const colorChange = ({ hex }) => hex = ColorChange(hex); // fonction change la couleur du pinceau
+  const colorChange = ({hex}) => {
+    hex = ColorChange(hex); // fonction change la couleur du pinceau
+  }
+  return (
+    <div class="background">
+      <div class="wrapper">
+          <script src="https://unpkg.com/peerjs@1.3.1/dist/peerjs.min.js"></script>
+
+          <div class="titre">
+            TITRE !
+          </div>
+          <div class="drawbox">
+            <canvas id="DrawBox" width="750" height="750"></canvas>
+          </div>
+
+          <div class="outils">
+            <button class="styleB" id="Pen+">Pen+</button>
+            <button class="styleB" id="Pen-">Pen-</button>
+            <button class="styleB" id="Pinceau">Pen</button>
+            <button class="styleB" id="Erase">Eraser</button>
+            <button class="styleB" id="Clear">Clear</button>
+
+            <button class="styleB" >...</button>
+            <button class="styleB" >...</button>
+            <button class="styleB" >...</button>
+            <button class="styleB" >...</button>
+            <button class="styleB" >...</button>
+
+          </div>
+
+          <div class="couleurs">
+            <CirclePicker
+              onChange={colorChange}
+              width = "0"
+            />
+          </div>
+          <div class="chatbox">
+            Chat
+          </div>
+          <div class="score">
+            Score
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/index.js
@@ -0,0 +1,125 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reactCSS from 'reactcss'
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import App from './App'
+//import Peer from 'peerjs';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+//Canvas
+var canvas = document.getElementById('DrawBox');
+var context = canvas.getContext('2d');
+
+//Pen
+var color = document.getElementsByClassName('color');
+var penUp = document.getElementById('Pen+');
+var penDown = document.getElementById('Pen-');
+var erase = document.getElementById('Erase');
+var pinceau = document.getElementById('Pinceau');
+var clear = document.getElementById('Clear');
+context.lineWidth = 8;
+var isDrawing;
+
+//---------------------------------------------------------------------------
+//---------------------------------------------------------------------------
+//---------------------------------------------------------------------------
+/*
+//Fonction de PeerJS
+window.addEventListener('load', (event) =>{
+  var peer = new Peer();
+  var conn = peer.connect('another-peers-id');
+
+  conn.on('open', () => {
+    conn.send('hi!');
+  });
+
+  peer.on('connection', (conn) => {
+    conn.on('data', (data) => {
+      // Will print 'hi!'
+      console.log(data);
+    });
+    conn.on('open', () => {
+      conn.send('hello!');
+    });
+  });
+});
+
+*/
+//---------------------------------------------------------------------------
+//---------------------------------------------------------------------------
+//---------------------------------------------------------------------------
+
+//Fonction sur le pinceau
+export function ColorChange(hex){
+    isDrawing = false;
+    context.strokeStyle = hex; // change la couleur du trait
+}
+
+penUp.onclick = function() {
+    isDrawing = false;
+    context.lineWidth += 5;
+};
+
+penDown.onclick = function() {
+    isDrawing = false;
+    context.lineWidth -= 5;
+};
+
+erase.onclick = function() {
+    isDrawing = false;
+    context.strokeStyle = "white";
+};
+
+pinceau.onclick = function() {
+    isDrawing = false;
+    context.strokeStyle = "black";
+};
+
+clear.onclick = function() {
+    isDrawing = false;
+    context.clearRect(0,0,1000,1000);
+    context.strokeStyle = "black";
+};
+
+//Fonction de dessin
+function getMousePos(canvas, mouse) {
+    var rect = canvas.getBoundingClientRect(); // retourne sa position par rapport à la zone d'affichage.
+    return {
+      x: mouse.clientX - rect.left, // position en X de la souris - ditance depuis la droite de la fenêtre par rapport à la zone  de dessin.
+      y: mouse.clientY - rect.top // position en Y de la souris - ditance depuis le haut de la fenêtre par rapport à la zone de dessin.
+    };
+}
+
+canvas.onmousedown = function(mouse) { //on commence le dessin
+  var pos = getMousePos(canvas, mouse); // position (x,y) du crayon
+  isDrawing = true;
+  context.beginPath(); // commencer un nouveau trait
+  context.moveTo(pos.x, pos.y); //déplacer le crayon avec la méthode à la nouvelle position
+};
+
+canvas.onmouseup = function() { //on arrete le dessin
+  isDrawing = false;
+};
+
+canvas.onmousemove = function(mouse) {
+  if (isDrawing) {
+    var pos = getMousePos(canvas, mouse); // position (x,y) du crayon
+    context.fillStyle = 'blue';
+    context.lineTo(pos.x, pos.y); // dessiner une ligne
+    context.linecap = 'round'
+    context.stroke();
+  }
+};
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
