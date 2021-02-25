bf9e28f168f824353ff7f3a4d023ebf13c2f9fd8
D32mond <kevin.domingues@etu.univ-lyon1.fr>
2021-02-25 18:21:37 +0100

    Essai de prévisualisation des formes et fonction de download

--- Test/Fonction.js
+++ Test/Fonction.js
@@ -7,22 +7,44 @@ function getMousePos(canvas, mouse) {
 }
 
 var canvas = document.getElementById('test');
+var ctx = canvas.getContext('2d');
 
 var posInit;
 var posEnd;
+var des=false;
+
+canvas.onload = function (){
+  ctx.fillStyle("white");
+  ctx.fillRect(0,0,700,700);
+}
 
 canvas.onmousedown = function draw(mouse) { //On commence le dessin
-  if (canvas.getContext) {
-    var ctx = canvas.getContext('2d');
     posInit=getMousePos(canvas,mouse);
-  }
+    des=true;
 };
 
 
 canvas.onmouseup = function(mouse) { //on arrete le dessin
-  posEnd=getMousePos(canvas,mouse);
-  if (canvas.getContext) {
-    var ctx = canvas.getContext('2d');
+  if (des)
+  {
+    posEnd=getMousePos(canvas,mouse);
     ctx.strokeRect(posInit.x, posInit.y, posEnd.x - posInit.x, posEnd.y - posInit.y);
+    des=false;
   }
+};
+
+canvas.onmousemove = function (mouse) {
+  if (des)
+  {
+    var posEnd = getMousePos(canvas, mouse); // position (x,y) du crayon
+    ctx.clearRect(posInit.x, posInit.y, posEnd.x - posInit.x, posEnd.y - posInit.y);
+    ctx.strokeRect(posInit.x, posInit.y, posEnd.x - posInit.x, posEnd.y - posInit.y)
+  }
+};
+
+
+async function downloadCanvas(el) //Fonction de download du canvas en jpg
+{
+  const imageURI = canvas.toDataURL("image/jpg");
+  el.href = imageURI;
 };
