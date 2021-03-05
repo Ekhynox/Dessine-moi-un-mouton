484443b7a0e14617caeb3d7af3cc5a8de5e827e4
D32mond <kevin.domingues@etu.univ-lyon1.fr>
2021-03-05 12:43:56 +0100

    maj du site sur ma branche

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import './App.css';
 import React, {useState} from 'react';
 import reactCSS from 'reactcss';
+import 'bootstrap/dist/css/bootstrap.css';
+// React Color
 import {CirclePicker} from 'react-color';
-import {ColorChange} from './index';
-import {Connexion} from './connexion';
-import {DownloadCanvas} from './index';
-import { BiSave } from 'react-icons/bi';
-import { FaPaintBrush } from 'react-icons/fa';
-import { BiEraser } from 'react-icons/bi';
-import { BiRectangle } from 'react-icons/bi';
-import { BsCircle } from 'react-icons/bs';
+// React Icons
+import { BiSave, BiEraser, BiSend} from 'react-icons/bi';
+import { FaPaintBrush, FaTrashAlt, FaCircle, FaSquare } from 'react-icons/fa';
+import { BsCircle, BsSquare } from 'react-icons/bs';
 import { SiCurl } from "react-icons/si";
-import { FaTrashAlt } from 'react-icons/fa';
 import { VscPaintcan } from 'react-icons/vsc';
-
+import { RiArrowGoBackFill } from "react-icons/ri";
+// fonction
+import {ColorChange, DownloadCanvas} from './index';
+import {Connexion} from './index';
+import {Send} from './index.js';
 
 function App() {
   //const colorChange = ({ hex }) => hex = ColorChange(hex); // fonction change la couleur du pinceau
@@ -22,46 +23,79 @@ function App() {
   }
 
   return (
+<html>
+	<head>
+	</head>
+    <body>
     <div className="background">
       <div className="wrapper">
           <div className="titre">
-
+            <h3 id="show-peer"></h3>
+            <input id="peerID"></input>
+            <button onClick={Connexion}> Connexion </button>
           </div>
           <div className="drawbox">
-            <canvas id="DrawBox" width="750" height="750"></canvas>
+            <canvas id="DrawBox" width="600" height="600"></canvas>
           </div>
           <div className="outils">
-            <button className="styleB" id="Pen+">Pen+</button>
-            <button className="styleB" id="Pen-">Pen-</button>
+            <button className="styleB tresgrand" id="Pen++"><FaCircle/></button>
+            <button className="styleB grand" id="Pen+"><FaCircle/></button>
+            <button className="styleB moyen" id="Pen"><FaCircle/></button>
+            <button className="styleB petit" id="Pen-"><FaCircle/></button>
             <button className="styleB" id="Pinceau"><FaPaintBrush/></button>
             <button className="styleB" id="Erase"><BiEraser/></button>
             <button className="styleB" id="Clear"><FaTrashAlt/></button>
 
-            <button className="styleB" id="drawRect"><BiRectangle/></button>
+            <button className="styleB" id="drawRect"><BsSquare/></button>
+            <button className="styleB" id="drawRectFull"><FaSquare/></button>
             <button className="styleB" id="drawCircle"><BsCircle/></button>
+            <button className="styleB" id="drawCircleFull"><FaCircle/></button>
             <button className="styleB" id="drawLine"><SiCurl/></button>
-            <button className="styleB" id="fill"><VscPaintcan/></button>
+            <button className="styleB" id="fillIn"><VscPaintcan/></button>
+            <button className="styleB" id="Bundo"><RiArrowGoBackFill/></button>
             <button className="styleB" download="example.jpg" id="saveImage"><BiSave/></button>
           </div>
           <div className="couleurs">
             <CirclePicker
               onChange={colorChange}
-              width = "0"
+              width = {1}
+              circleSize ={26}
+              colors = {["#e91e63", "#9c27b0", "#673ab7", "#3f51b5", "#2196f3", "#00bcd4", "#009688", "#4caf50", "#ffeb3b", "#ffc107", "#ff9800", "#795548", "#607d8b", "black", "white"]}
+
             />
           </div>
-          <div className="chatbox">
-            ChatTEST123545
+            <div className="card">
+              <div id="chatBox" className="card-body msg_card_body">
+
+                <div className="d-flex justify-content-start mb-4">
+                  <div className="msg_cotainer">
+                    Ekhynox : Hello toi !
+                    <span className="msg_time">46 Sec</span>
+                  </div>
+                </div>
+              </div>
+
+              <div className="card-footer">
+                <div className="input-group">
+                  <input id="message" className="form-control type_msg" placeholder="Type your message..."></input>
+                  <div className="input-group-append">
+                    <button id="send" onClick={Send} className="input-group-text send_btn" ><BiSend/></button>
+                  </div>
+                </div>
+            </div>
           </div>
+
           <div className="score">
             Score
           </div>
-      </div>
-      <div>
-        <h3 id="show-peer"></h3>
-        <input id="peerID"></input>
-        <button onClick={Connexion}> connection </button>
+
+          <div className="video">
+            <video autoPlay id="Video" width="600" height="600"></video>
+          </div>
       </div>
     </div>
+    </body>
+  </html>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import reportWebVitals from './reportWebVitals';
 import App from './App';
 import Peer from 'peerjs';
 
-
 ReactDOM.render(
   <React.StrictMode>
     <App/>
@@ -20,21 +19,147 @@ var context = canvas.getContext('2d');
 
 //Pen
 var color = document.getElementsByClassName('color');
-var penUp = document.getElementById('Pen+');
-var penDown = document.getElementById('Pen-');
 var erase = document.getElementById('Erase');
 var pinceau = document.getElementById('Pinceau');
 var clear = document.getElementById('Clear');
 var rect = document.getElementById('drawRect');
+var rectfull = document.getElementById('drawRectFull');
 var circle = document.getElementById('drawCircle');
+var circlefull = document.getElementById('drawCircleFull');
 var line = document.getElementById('drawLine');
+var fill = document.getElementById('fillIn');
+var undo = document.getElementById('Bundo');
 var save = document.getElementById('saveImage');
-context.lineWidth = 8;
+context.lineWidth = 6;
 var isDrawing;
 var drawPinceau = true;
 var drawRectangle = false;
+var drawRectangleFull = false;
 var drawCircle = false;
+var drawCircleFull = false;
 var drawLine = false;
+var fillIn = false;
+var pseudos = "Ekhynox";
+var time = 23;
+var sourceCanvas;
+
+
+//Fonction de PeerJS
+var peer = new Peer();
+var conn;
+var peerID;
+var connID;
+var stream = canvas.captureStream(300);
+
+//Mon ID peerJS
+peer.on('open', function(id) {
+  peerID = id;
+  document.getElementById("show-peer").innerHTML = peerID;
+});
+
+//Stream
+function VideoStream(myStream){
+  document.createElement('a');
+  var video = document.getElementById('Video');
+  video.srcObject = myStream;
+  console.log(video.srcObject);
+}
+
+//Connexion
+export function Connexion() {
+  connID = document.getElementById("peerID").value; //Id du host
+  document.getElementById("show-peer").innerHTML = "Connexion " + connID;
+  conn = peer.connect(connID);
+
+  var mystream = peer.call(connID, stream);
+  mystream.on('stream', function(remoteStream){
+    console.log("call on");
+    VideoStream(remoteStream);
+  });
+}
+
+peer.on('call', function(call) {
+  call.answer(stream); // Answer the call with an A/V stream.
+});
+
+
+//Envoyer des messages
+export function Send() {
+  var chatBox = document.getElementById("chatBox")
+  var div1 = document.createElement("div");
+  var div2 = document.createElement("div");
+  var span = document.createElement("span");
+  var message = document.getElementById("message");
+  conn = peer.connect(connID);
+
+  connID = document.getElementById("peerID").value; //Id du host
+  document.getElementById("show-peer").innerHTML = "Connexion " + connID;
+
+  div1.setAttribute("class", "d-flex justify-content-start mb-4");
+  div2.setAttribute("class", "msg_cotainer");
+  span.setAttribute("class", "msg_time");
+
+  div2.innerHTML = pseudos + " : " + message.value;
+  document.getElementById("message").value = "";
+
+  div2.appendChild(span);
+  div1.appendChild(div2);
+  chatBox.appendChild(div1);
+
+  peer.on('open', function(id) {
+    peerID = id;
+    console.log(peerID);
+  });
+
+  conn.on('open', function(id) {
+    conn.send(pseudos + " : " + message.value);
+  });
+}
+
+
+//Reception
+peer.on('connection', function(conn) {
+  conn.on('data', function(data){
+
+    var chatBox = document.getElementById("chatBox")
+    var div1 = document.createElement("div");
+    var div2 = document.createElement("div");
+    var span = document.createElement("span");
+
+    div1.setAttribute("class", "d-flex justify-content-start mb-4");
+    div2.setAttribute("class", "msg_cotainer");
+    span.setAttribute("class", "msg_time");
+
+    div2.innerHTML = data;
+
+    div2.appendChild(span);
+    div1.appendChild(div2);
+    chatBox.appendChild(div1);
+
+    console.log(data);
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+//initialise le canvas avec un fond blanc et les outils sont de couleur noir de base
+function CanvasInit(){
+  context.fillStyle = 'white';
+  context.fillRect(0,0,canvas.width, canvas.height);
+  context.fillStyle = 'black';
+  context.strokeStyle = 'black'
+}
+CanvasInit();
 
 //Fonction sur le pinceau
 export function ColorChange(hex){
@@ -43,19 +168,39 @@ export function ColorChange(hex){
     context.fillStyle = hex;
 }
 
-penUp.onclick = function() {
+var verybigPen = document.getElementById('Pen++');
+var bigPen = document.getElementById('Pen+');
+var smallPen = document.getElementById('Pen');
+var verysmallPen = document.getElementById('Pen-');
+
+verybigPen.onclick = function() {
     isDrawing = false;
-    context.lineWidth += 5;
+    context.lineWidth = 18;
 };
 
-penDown.onclick = function() {
+bigPen.onclick = function() {
     isDrawing = false;
-    context.lineWidth -= 5;
+    context.lineWidth = 12;
+};
+
+smallPen.onclick = function() {
+    isDrawing = false;
+    context.lineWidth = 6;
+};
+
+verysmallPen.onclick = function() {
+    isDrawing = false;
+    context.lineWidth = 2;
 };
 
 erase.onclick = function() {
     drawPinceau = true;
     drawRectangle = false;
+    drawRectangleFull = false;
+    drawCircle = false;
+    drawCircleFull=false;
+    drawLine = false;
+    fillIn = false;
     isDrawing = false;
     context.strokeStyle = "white";
 };
@@ -64,58 +209,107 @@ pinceau.onclick = function() {
     isDrawing = false;
     drawPinceau = true;
     drawRectangle = false;
+    drawRectangleFull = false;
     drawCircle = false;
+    drawCircleFull=false;
+    drawLine = false;
+    fillIn = false;
     context.strokeStyle = "black"
 };
 
 clear.onclick = function() {
+    sourceCanvas = canvas.toDataURL('image/jpeg', 1.0);
     drawPinceau = true;
     drawRectangle = false;
+    drawRectangleFull = false;
+    drawCircle = false;
+    drawCircleFull=false;
+    drawLine = false;
+    fillIn = false;
     isDrawing = false;
-    context.clearRect(0,0,1000,1000);
-    context.strokeStyle = "black";
+    context.fillStyle = 'white';
+    context.fillRect(0,0,canvas.width, canvas.height);
+    context.fillStyle = 'black';
+    // context.strokeStyle = "black";
 };
 
 rect.onclick = function() {
     drawPinceau = false;
     drawRectangle = true;
+    drawRectangleFull = false;
     drawCircle = false;
+    drawCircleFull=false
     drawLine = false;
-    //context.strokeStyle = "black";
+    fillIn = false;
+    // context.strokeStyle = "black";
+};
+
+rectfull.onclick = function() {
+    drawPinceau = false;
+    drawRectangle = false;
+    drawRectangleFull = true;
+    drawCircle = false;
+    drawCircleFull=false;
+    drawLine = false;
+    fillIn = false;
+    // context.fillStyle = "black";
 };
 
 circle.onclick = function() {
     drawPinceau = false;
     drawRectangle = false;
+    drawRectangleFull = false;
     drawCircle = true;
+    drawCircleFull=false;
     drawLine = false;
-    //context.strokeStyle = "black";
+    fillIn = false;
+    // context.strokeStyle = "black";
+};
+
+circlefull.onclick = function() {
+    drawPinceau = false;
+    drawRectangle = false;
+    drawRectangleFull = false;
+    drawCircle = false;
+    drawCircleFull=true;
+    drawLine = false;
+    fillIn = false;
+    // context.fillStyle = "black";
 };
 
 line.onclick = function() {
     drawPinceau = false;
     drawRectangle = false;
+    drawRectangleFull = false;
     drawCircle = false;
+    drawCircleFull=false;
     drawLine = true;
-    //context.strokeStyle = "black";
+    fillIn = false;
+    // context.strokeStyle = "black";
 }
 
-//crée un fond blanc
-canvas.onload = function (){
-  context.fillStyle("white");
-  context.fillRect(0,0,750,750);
+fill.onclick = function()
+{
+  drawPinceau = false;
+  drawRectangle = false;
+  drawRectangleFull = false;
+  drawCircle = false;
+  drawCircleFull=false;
+  drawLine = false;
+  fillIn = true;
 }
 
+undo.onclick = function() {
+  drawDataURIOnCanvas(sourceCanvas);
+}
 
 //Enregister une image
 save.onclick = function() {
   var img = document.createElement('a');
-  img.href = canvas.toDataURL("image/jpg").replace('png','jpg');
+  img.href = canvas.toDataURL('image/jpg', 1.0); //premier param demande d'être en jpg , le deuxieme dit que la conversion ce fait en HD.
   img.download = ('dessine-moi.jpg');
   img.click();
-  console.log("bleh");
 }
-
 
 //Fonction de dessin
 function getMousePos(canvas, mouse) {
@@ -130,31 +324,50 @@ var posInit;
 var posEnd;
 
 canvas.onmousedown = function (mouse) { //on commence le dessin
+  sourceCanvas = canvas.toDataURL('image/jpeg', 1.0);
   if (drawPinceau)
   {
-    var pos = getMousePos(canvas, mouse); // position (x,y) du crayon
+    posInit = getMousePos(canvas, mouse); // position (x,y) du crayon
     isDrawing = true;
+
+    context.beginPath();
+    context.arc(posInit.x, posInit.y, context.lineWidth/2, 0, 2 * Math.PI, false);
+    context.fill();
+
     context.beginPath(); // commencer un nouveau trait
-    context.moveTo(pos.x, pos.y); //déplacer le crayon avec la méthode à la nouvelle position
+    context.lineCap = 'round';
+    context.moveTo(posInit.x, posInit.y); //déplacer le crayon avec la méthode à la nouvelle position
   }
   if (drawRectangle)
   {
     posInit=getMousePos(canvas,mouse);
     isDrawing = true;
-    context.beginPath();
+  }
+  if (drawRectangleFull)
+  {
+    posInit=getMousePos(canvas,mouse);
+    isDrawing = true;
   }
   if(drawCircle)
   {
     posInit=getMousePos(canvas,mouse);
     isDrawing = true;
-    context.beginPath();
+  }
+  if(drawCircleFull)
+  {
+    posInit=getMousePos(canvas,mouse);
+    isDrawing = true;
   }
   if(drawLine)
   {
     posInit=getMousePos(canvas,mouse);
     isDrawing = true;
+  }
+  if(fillIn)
+  {
+    posInit=getMousePos(canvas,mouse);
+    isDrawing = true;
     context.beginPath();
-    context.moveTo(posInit.x, posInit.y);
   }
 };
 
@@ -162,20 +375,44 @@ canvas.onmouseup = function (mouse) { //on arrete le dessin
   if (isDrawing  && drawRectangle)
   {
     posEnd=getMousePos(canvas,mouse);
+    context.beginPath();
     context.strokeRect(posInit.x, posInit.y, posEnd.x - posInit.x, posEnd.y - posInit.y);
+  }
+  if (isDrawing  && drawRectangleFull)
+  {
+    posEnd=getMousePos(canvas,mouse);
+    context.beginPath();
+    context.fillRect(posInit.x, posInit.y, posEnd.x - posInit.x, posEnd.y - posInit.y);
   }
 
   if(isDrawing && drawCircle)
   {
     posEnd=getMousePos(canvas,mouse);
-    context.ellipse(posInit.x, posInit.y, Math.abs(posEnd.x - posInit.x), Math.abs(posEnd.y - posInit.y), 0, 0, 2*3.14);
+    context.beginPath();
+    context.ellipse(posInit.x, posInit.y, Math.abs(posEnd.x - posInit.x), Math.abs(posEnd.y - posInit.y), 0, 0, 2*Math.PI);
     context.stroke();
   }
+  if(isDrawing && drawCircleFull)
+  {
+    posEnd=getMousePos(canvas,mouse);
+    context.beginPath();
+    context.ellipse(posInit.x, posInit.y, Math.abs(posEnd.x - posInit.x), Math.abs(posEnd.y - posInit.y), 0, 0, 2*Math.PI);
+    context.fill();
+  }
+
   if(isDrawing && drawLine)
   {
     posEnd=getMousePos(canvas,mouse);
+    context.beginPath();
+    context.moveTo(posInit.x, posInit.y);
     context.lineTo(posEnd.x, posEnd.y);
     context.stroke();
+  }
+  if(isDrawing && fillIn)
+  {
+    posEnd=getMousePos(canvas,mouse);
+    //context.fillStyle;
+    console.log("blablabla");
   }
   isDrawing = false;
 };
@@ -184,11 +421,60 @@ canvas.onmousemove = function (mouse) {
   if (isDrawing && drawPinceau) {
     var pos = getMousePos(canvas, mouse); // position (x,y) du crayon
     context.lineTo(pos.x, pos.y); // dessiner une ligne
-    context.linecap = 'round'
+    context.stroke();
+  }
+
+  if (isDrawing && drawRectangle)
+  {
+    drawDataURIOnCanvas(sourceCanvas);
+    posEnd = getMousePos(canvas, mouse); // position (x,y) du crayon
+    context.beginPath();
+    context.strokeRect(posInit.x, posInit.y, posEnd.x - posInit.x, posEnd.y - posInit.y);
+  }
+  if (isDrawing && drawRectangleFull)
+  {
+    drawDataURIOnCanvas(sourceCanvas);
+    posEnd = getMousePos(canvas, mouse); // position (x,y) du crayon
+    context.beginPath();
+    context.fillRect(posInit.x, posInit.y, posEnd.x - posInit.x, posEnd.y - posInit.y);
+  }
+
+  if(isDrawing && drawCircle)
+  {
+    drawDataURIOnCanvas(sourceCanvas);
+    posEnd=getMousePos(canvas,mouse);
+    context.beginPath();
+    context.ellipse(posInit.x, posInit.y, Math.abs(posEnd.x - posInit.x), Math.abs(posEnd.y - posInit.y), 0, 0, 2*Math.PI);
+    context.stroke();
+  }
+  if(isDrawing && drawCircleFull)
+  {
+    drawDataURIOnCanvas(sourceCanvas);
+    posEnd=getMousePos(canvas,mouse);
+    context.beginPath();
+    context.ellipse(posInit.x, posInit.y, Math.abs(posEnd.x - posInit.x), Math.abs(posEnd.y - posInit.y), 0, 0, 2*Math.PI);
+    context.fill();
+  }
+
+  if(isDrawing && drawLine)
+  {
+    drawDataURIOnCanvas(sourceCanvas);
+    posEnd=getMousePos(canvas,mouse);
+    context.beginPath();
+    context.moveTo(posInit.x, posInit.y);
+    context.lineTo(posEnd.x, posEnd.y);
     context.stroke();
   }
 };
 
+
+function drawDataURIOnCanvas(strDataURI) {  //elle prend en param l'url d'une image, ici le cache du canvas et le draw.
+  var img2 = new window.Image();
+  img2.addEventListener("load", function () {
+      canvas.getContext("2d").drawImage(img2, 0, 0,canvas.width, canvas.height);
+  });
+  img2.setAttribute("src", strDataURI);
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
