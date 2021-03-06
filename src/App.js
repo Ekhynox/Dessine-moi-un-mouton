9138aa18a54c8f33bc0416eb5f31197d6cc7f191
Ekhynox <60545676+Ekhynox@users.noreply.github.com>
2021-03-06 23:01:05 +0100

    css 1.1

--- src/App.js
+++ src/App.js
@@ -55,7 +55,7 @@ function App() {
               onChange={colorChange}
               width = {1}
               circleSize ={26}
-              colors = {["#e91e63", "#9c27b0", "#673ab7", "#3f51b5", "#2196f3", "#00bcd4", "#009688", "#4caf50", "#ffeb3b", "#ffc107", "#ff9800", "#795548", "#607d8b", "black", "white"]}
+              colors = {["#f44336", "#e91e63", "#9c27b0", "#673ab7", "#3f51b5", "#2196f3", "#03a9f4", "#00bcd4", "#009688", "#4caf50", "#8bc34a", "#cddc39", "#ffeb3b", "#ffc107", "#ff9800", "#ff5722", "#795548", "#607d8b", "black", "white"]}
 
             />
           </div>
