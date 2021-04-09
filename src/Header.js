d548291f02fdbf1e297fa80cbf4dff8dff310944
Chocoluna <cl.coutupetra@gmail.com>
2021-04-09 19:51:35 +0200

    Barre de navigation : 1.2

--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,8 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import MenuIcon from '@material-ui/icons/Menu';
 import { FiSun, FiMoon } from "react-icons/fi";
-import {AppBar, Button, ButtonGroup, IconButton, FormControlLabel, FormGroup, Menu, MenuItem, Switch, Toolbar, Typography} from '@material-ui/core';
+import {AppBar, Button, ButtonGroup, IconButton, FormControlLabel, FormGroup, Grid, Menu, MenuItem, Switch, Toolbar, Typography} from '@material-ui/core';
+import sheep from './img/sheep.png';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -20,6 +21,17 @@ const useStyles = makeStyles((theme) => ({
   title: {
     flexGrow: 1,
   },
+
+  Button:{
+    fontFamily: 'Barlow, san-serif',
+    fontSize: '200%',
+    fontWeight: 'bold',
+    width: '40%',
+    height: '15%',
+    padding: '0 30px',
+    margin : theme.spacing ('auto', 'auto'),
+  },
+
 }));
 
 function MenuAppBar() {
@@ -42,24 +54,28 @@ function MenuAppBar() {
 
   return (
     <div className={classes.root}>
-      <AppBar position="relative">
+      <AppBar position="relative" style={{backgroundColor: '#000051'}}>
         <Toolbar>
           <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
             <MenuIcon />
           </IconButton>
+          <img src={sheep} />
           <ButtonGroup variant="text"  aria-label="text primary button group" aria-controls="menu-appbar"
           aria-haspopup="true"
           onClick={handleMenu}
-          color="inherit">
+          color="inherit"
+          className={classes.Button}>
           <Button> About Us </Button>
           <Button> Help </Button>
           </ButtonGroup>
-          <FormGroup>
-            <FormControlLabel
-              control={<Switch checked={auth} onChange={handleChange} aria-label="login switch" />}
-              label={auth ? <FiSun /> : <FiMoon/>}
-            />
-          </FormGroup>
+          <Grid container justify="flex-end">
+           <FormGroup>
+             <FormControlLabel edge = "end"
+               control={<Switch checked={auth} onChange={handleChange} aria-label="login switch" style={{color: '#ffffcf'}} />}
+               label={auth ? <FiSun /> : <FiMoon/>}
+             />
+           </FormGroup>
+           </Grid>
           {auth && (
             <div>
               <Menu
